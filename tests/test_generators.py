286889c19a947573feff4ca6313b90986ec5dc89
Sertalp B. Cay <sertalp.cay@sas.com>
2018-10-31 15:29:38 -0400

    Unittests with hashed values are working

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -22,7 +22,8 @@ This test file generates intermediate forms to run optimization models using SAS
 
 import unittest
 import os
-import sasoptpy
+import sys
+import sasoptpy as so
 import tests.responses as expected
 import hashlib
 hash = hashlib.sha256
@@ -50,21 +51,159 @@ class MockCASServer:
         pass
 
 
+def mock_solve(model, *args, **kwargs):
+    """
+    Overrides :meth:`Model.solve` method to be used in unittests.
+
+    Parameters
+    ----------
+    model : :class:`Model` object
+        Model to be used in unittest
+    args
+        Arguments
+    kwargs
+        Keyword arguments
+    ctr : integer
+        Counter for the multi-response tests
+    expected : list
+        List of hash values of the generated OPTMODEL strings
+
+    Returns
+    -------
+    boolean
+        Success (True) or Failure (False) for the current test
+    """
+    global ctr
+    global expected_response
+    global tests
+    global records
+    success = True
+    print('Mock solve is called!')
+    if 'verbose' in kwargs:
+        del kwargs['verbose']
+    optmodel_code = model.to_optmodel(**kwargs)
+    obtained = hash(optmodel_code.encode()).hexdigest()
+    records.append(obtained)
+    if obtained == expected_response[ctr]:
+        print('Success!', obtained, expected_response[ctr])
+    else:
+        success = False
+        print('Failed!')
+        print(obtained, expected_response[ctr])
+    ctr += 1
+    tests.append(success)
+    return success
+
+
 class TestGenerators(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.server = MockCASServer()
 
+    def setUp(self):
+        self.old_stdout = sys.stdout
+        sys.stdout = NullWriter()
+        sys.stderr = NullWriter()
+
     def tearDown(self):
-        sasoptpy.reset_globals()
+        so.reset_globals()
+
+    def check_results(self):
+        sys.stdout = self.old_stdout
+        print('Problem:', self.problem)
+        for i, v in enumerate(self.results):
+            print('Model', i, v, records[i])
+            self.assertTrue(v)
+
+    def set_expectation(self, problem, test):
+        global ctr
+        global expected_response
+        global tests
+        global records
+        ctr = 0
+        expected_response = test
+        self.results = tests = []
+        self.problem = problem
+        self.records = records = []
 
     def test_fm1(self):
+        self.set_expectation('Food Manufacture 1', expected.fm1)
         from examples.food_manufacture_1 import test
-        st = test(TestGenerators.server, solve=False)
-        print(hash(st))
-        #print('***', st, '***')
-        #print('***', expected.fm1, '***')
-        #self.assertMultiLineEqual(st, expected.fm1)
+        test(TestGenerators.server)
+        self.check_results()
+
+    def test_fm2(self):
+        self.set_expectation('Food Manufacture 2', expected.fm2)
+        from examples.food_manufacture_2 import test
+        test(TestGenerators.server)
+        self.check_results()
+
+    def test_fp1(self):
+        self.set_expectation('Factory Planning 1', expected.fp1)
+        from examples.factory_planning_1 import test
+        test(TestGenerators.server)
+        self.check_results()
+
+    def test_fp2(self):
+        self.set_expectation('Factory Planning 2', expected.fp2)
+        from examples.factory_planning_2 import test
+        test(TestGenerators.server)
+        self.check_results()
+
+    def test_mp(self):
+        self.set_expectation('Manpower Planning', expected.mp)
+        from examples.manpower_planning import test
+        test(TestGenerators.server)
+        self.check_results()
+
+    def test_ro(self):
+        self.set_expectation('Refinery Optimization', expected.ro)
+        from examples.refinery_optimization import test
+        test(TestGenerators.server)
+        self.check_results()
+
+    def test_mo(self):
+        self.set_expectation('Mining Optimization', expected.mo)
+        from examples.mining_optimization import test
+        try:
+            test(TestGenerators.server)
+        except ZeroDivisionError:
+            pass
+        self.check_results()
+
+    def test_farmp(self):
+        self.set_expectation('Farm Planning', expected.farmp)
+        from examples.farm_planning import test
+        test(TestGenerators.server)
+        self.check_results()
+
+    def test_econ(self):
+        self.set_expectation('Economic Planning', expected.econ)
+        from examples.economic_planning import test
+        test(TestGenerators.server)
+        self.check_results()
+
+    def test_dc(self):
+        self.set_expectation('Decentralization', expected.dc)
+        from examples.decentralization import test
+        test(TestGenerators.server)
+        self.check_results()
+
+    def test_ow(self):
+        self.set_expectation('Optimal Wedding', expected.ow)
+        from examples.sas_optimal_wedding import test
+        test(TestGenerators.server)
+        self.check_results()
+
+    def test_kx(self):
+        self.set_expectation('Kidney Exchange', expected.kx)
+        from examples.sas_kidney_exchange import test
+        test(TestGenerators.server)
+        self.check_results()
+
 
 if __name__ == '__main__':
+    globals()['so'].Model.solve = mock_solve
     unittest.main()
+
+
