7cb6876608deaf491022d7c30695bf35159480f4
Sertalp B. Cay <sertalp.cay@sas.com>
2019-06-25 15:24:13 -0400

    Added objective class

---
+++ sasoptpy/core/objective.py
@@ -0,0 +1,25 @@
+import sasoptpy
+from sasoptpy.core import Expression
+
+
+class Objective(Expression):
+    """
+    """
+
+    def __init__(self, exp, name, sense=None):
+        super().__init__(exp=exp)
+        self._name = sasoptpy.util.assign_name(name, 'obj')
+        self._objorder = sasoptpy.util.register_globally(self._name, self)
+        if sense is None:
+            sense = sasoptpy.MIN
+        self._sense = sense
+
+    def _defn(self):
+        return self._sense.lower() + ' ' + \
+               self._name + ' = ' + self._expr() + ';'
+
+    def get_sense(self):
+        return self._sense
+
+    def set_sense(self, sense):
+        self._sense = sense
