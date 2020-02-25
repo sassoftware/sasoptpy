07644f363f395ac43d35119679883ec046bc7ea4
Sertalp B. Cay <sertalp.cay@sas.com>
2020-02-25 09:57:46 -0500

    Added pypi setup for CI/CD

--- setup.py
+++ setup.py
@@ -35,8 +35,8 @@ setup(
     author_email='Sertalp.Cay@sas.com',
     license='Apache v2.0',
     install_requires=[
-        'pandas >= 0.25.1',
-        'swat >= 1.4.0',
+        'pandas >= 1.0.0',
+        'swat >= 1.6.1',
         'saspy >= 3.1.0',
         'numpy >= 1.15.4'
         ],
