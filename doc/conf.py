9ee441181d295c2635eeb5a8f7d1e986ec1de0c0
Sertalp Cay <Sertalp.Cay@SAS.com>
2021-08-31 16:41:58 -0400

    Fix for doc methods

--- doc/conf.py
+++ doc/conf.py
@@ -263,7 +263,7 @@ def remove_autosums(app, what, name, obj, options, lines):
 def setup(app):
     app.connect('autodoc-process-docstring', remove_autosums)
     app.add_css_file('css/sasoptpy.css') 
-    app.add_javascript('js/url.js')
+    app.add_js_file('js/url.js')
 
 add_module_names = False
 
